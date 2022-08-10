0e2cc948052deb3f0e7a62fa33c8bfef189350ee
AmerikanPsyko <geekdouttechnology@gmail.com>
2022-08-10 13:25:22 -0700

    added component pages
    profile not finished, need nav bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,30 +18,55 @@ import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { FormsModule } from '@angular/forms';
 import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
 import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
+import { MovieCardComponent } from './movie-card/movie-card.component';
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
+import { RouterModule, Routes } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { DirectorComponent } from './director/director.component';
+import { GenreComponent } from './genre/genre.component';
+import { SynopsisComponent } from './synopsis/synopsis.component';
+import { ProfileComponent } from './profile/profile.component';
 
+const appRoutes: Routes = [
+  { path: 'welcome', component: WelcomePageComponent},
+  { path: 'movies', component: MovieCardComponent},
+  { path: '', redirectTo: 'welcome', pathMatch: 'prefix'},
+  
+];
 
 
 @NgModule({
   declarations: [
    AppComponent,
    UserRegistrationFormComponent,
-   UserLoginFormComponent
+   UserLoginFormComponent,
+   MovieCardComponent,
+   WelcomePageComponent,
+   DirectorComponent,
+   GenreComponent,
+   SynopsisComponent,
+   ProfileComponent,
+  
   ],
   imports: [
+    HttpClientModule,
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule,
     BrowserAnimationsModule,
-    FormsModule,
-    MatDialogModule,
     MatInputModule,
     MatButtonModule,
     MatCardModule,
     MatFormFieldModule,
+    MatDialogModule,
     MatSnackBarModule,
+    MatIconModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes),
    
   ],
   providers: [],
   bootstrap: [AppComponent]
+
+  
 })
 export class AppModule { }
